875ae36b14060af51477daea7f83c70f5ee6fdc7
student <student@CR5414>
2018-02-16 17:50:38 +0200

    2d array demo

--- src/org/tyaa/arraysdemo/ArraysDemo.java
+++ src/org/tyaa/arraysdemo/ArraysDemo.java
@@ -15,7 +15,36 @@ public class ArraysDemo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        int x = 10;
+        
+        int[] arr1 = new int[10];
+        
+        arr1[0] = 1;
+        arr1[1] = 3;
+        arr1[2] = 5;
+        
+        //System.out.println(x);
+        //System.out.println(arr1[0]);
+        //System.out.println(arr1[3]);
+        
+        /*for (int i = 0; i < arr1.length; i++) {
+            //System.out.println(Math.random() * 100);
+            arr1[i] = (int) (Math.random() * 100);
+            //arr1[i] = Integer.parseInt(String.valueOf(Math.random()));
+        }
+        
+        for (int i = 0; i < arr1.length; i++) {
+            //System.out.println(i);
+            System.out.println(arr1[i]);
+        }*/
+        
+        int[][] t = new int[10][10];
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                t[i][j] = i * j;
+            }
+        }
     }
     
 }
